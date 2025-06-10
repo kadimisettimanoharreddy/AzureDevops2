c35665852702fa0b21998184847d655365f53ad3
kadimisettimanoharreddy <manoharkadimisetti2@gmail.com>
2025-06-10 19:23:35 +0530

    my modified mobile_app

--- src/main/java/com/mobile/app/Jdbc.java
+++ src/main/java/com/mobile/app/Jdbc.java
@@ -1,32 +1,21 @@
 package com.mobile.app;
 
-import java.io.FileReader;
-
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class Jdbc {
-    static Connection jdbcconnection() throws ClassNotFoundException, SQLException, IOException {
-        Properties props = new Properties();
+    static Connection jdbcconnection() throws ClassNotFoundException, SQLException {
         
-        // Load properties file using FileReader
-        FileReader reader = new FileReader("C:\\Users\\MANOHAR REDDY\\Downloads\\dbb.properties");
-        props.load(reader);
-        reader.close();
+        String url = "jdbc:mysql://localhost:3306/newdb";  // Your database name
+        String user = "root";       // Assuming root user
+        String password = "Manohar6464";   // Your password
         
-        // Retrieve values from properties file
-        String url = props.getProperty("db.url");
-        String user = props.getProperty("db.user");
-        String password = props.getProperty("db.password");
-
-        // Establish the database connection
         Class.forName("com.mysql.cj.jdbc.Driver");
+        
         Connection con = DriverManager.getConnection(url, user, password);
         System.out.println("Connection created successfully");
-
+        
         return con;
     }
 }
